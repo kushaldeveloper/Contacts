00503045bb2a15a963949eba52b831fa06ffbea6
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-19 17:14:25 +0530

    add

--- www/js/funct.js
+++ www/js/funct.js
@@ -1,8 +1,10 @@
 	document.addEventListener("deviceready",onDeviceReady,false);
-	document.getElementById("createContact").addEventListener("click", createContact);
 	function onDeviceReady(){
 		alert("device is starting");
-	function createContact() {
+		document.getElementById("createContact").addEventListener("click", createContact);
+		document.getElementById("findContact").addEventListener("click", findContact);
+		document.getElementById("deleteContact").addEventListener("click", deleteContact);
+		function createContact() {
 			var myContact = navigator.contacts.create({"displayName": "Unknown"});
 			myContact.save(contactSuccess, contactError);
     
@@ -15,4 +17,48 @@
 		}
 	
 	}
+		function findContacts() {
+			var options = new ContactFindOptions();
+			options.filter = "";
+			options.multiple = true;
+			fields = ["displayName"];
+			navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+    
+		function contactfindSuccess(contacts) {
+			for (var i = 0; i < contacts.length; i++) {
+			alert("Display Name = " + contacts[i].displayName);
+		}
+    }
+	
+		function contactfindError(message) {
+			alert('Failed because: ' + message);
+		}
+	
+	}
+		function deleteContact() {
+			var options = new ContactFindOptions();
+			options.filter = "Test User";
+			options.multiple = false;
+			fields = ["displayName"];
+	   navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+
+		function contactfindSuccess(contacts) {
+			var contact = contacts[0];
+			contact.remove(contactRemoveSuccess, contactRemoveError);
+
+      function contactRemoveSuccess(contact) {
+			alert("Contact Deleted");
+		}
+
+      function contactRemoveError(message) {
+			alert('Failed because: ' + message);
+		}
+   }
+
+   function contactfindError(message) {
+      alert('Failed because: ' + message);
+		}
+	
+	}
+	
 }			
