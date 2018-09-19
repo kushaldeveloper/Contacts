00530d8215e18010f21c8c03458c758fb990bc7a
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-19 16:46:14 +0530

    add

---
+++ www/js/funct.js
@@ -0,0 +1,18 @@
+document.addEventListener("deviceready",onDeviceReady,false);
+	function onDeviceReady(){
+		alert("device is starting");
+	document.getElementById("createContact").addEventListener("click", createContact);
+	}
+	function createContact() {
+			var myContact = navigator.contacts.create({"displayName": "Unknown"});
+			myContact.save(contactSuccess, contactError);
+    
+		function contactSuccess() {
+			alert("Contact is saved!");
+		}
+	
+		function contactError(message) {
+			alert('Failed because: ' + message);
+		}
+	
+	}	
