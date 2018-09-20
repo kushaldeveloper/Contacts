d7c0eaae7cdb20f70d25d0ce122421c8c69ef31e
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-20 16:23:23 +0530

    test6

--- www/js/funct.js
+++ www/js/funct.js
@@ -2,23 +2,18 @@
 	function onDeviceReady(){
 		alert("device is starting");
 		document.getElementById("createContact").addEventListener("click", createContact);
-	}
 		//document.getElementById("findContact").addEventListener("click", findContact);
 		//document.getElementById("deleteContact").addEventListener("click", deleteContact);
 		function createContact() {
-			var myContact = navigator.contacts.create({"displayName": "Unknown"});
-			alert('..');
-			myContact.save(contactSuccess, contactError);
-			alert('.....');
-    
-		function contactSuccess() {
+			var myContact = navigator.contacts.create({"displayName": "Test User"});
+		/*function contactSuccess() {
 			alert("Contact is saved!");
-		};
-	
+		}
 		function contactError(message) {
 			alert('Failed because: ' + message);
-		};
+		}*/
 	}
+}
 	
 
 		/*function findContacts() {
